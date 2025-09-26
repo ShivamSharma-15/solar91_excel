b3486f6374ccd7127a0e93d58c2cf3407953e5d4
Shivam <main.shivamsharma@gmail.com>
2025-09-26 17:38:24 +0530

    third commit

--- src/controller/cronController.js
+++ src/controller/cronController.js
@@ -4,5 +4,6 @@ const { logicPower } = require("../service/delegateLogicPower");
 async function cronController(req, res, next) {
   await delegator();
   await logicPower();
+  return res.json({ success: true });
 }
 module.exports = { cronController };
